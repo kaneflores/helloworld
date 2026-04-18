6c0d61b4a14ba3aa3ffe947b909e608fec5f65a8
kaneflores <kaneflores911@gmail.com>
2026-04-18 13:49:37 -0400

    orig

--- numbers.c
+++ numbers.c
@@ -2,25 +2,20 @@
 // %s a string of chars
 // %d integer
 int main(){
-    while (1)
-    {
-        int x, y, result;
+    int x, y, result;
 
-        printf("Please input a number:\n");
-        scanf("%d",&x);
-        printf("Please input another number:\n");
-        scanf("%d", &y);
+    printf("Please input a number:\n");
+    scanf("%d",&x);
+    printf("Please input another number:\n");
+    scanf("%d", &y);
 
-        if (x != 6 && y != 7) {
-            result = x + y;
-            printf("The result is %d\n", result);
-        }
-        else{
-            printf("67!!!!!!!!!!!!!!!!\n");
-        }
+    if (x != 6 && y != 7) {
+        result = x + y;
+        printf("The result is %d\n", result);
     }
-    
-    
+    else{
+        printf("67!!!!!!!!!!!!!!!!\n");
+    }
       
     
 
