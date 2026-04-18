c2cd8646b23eaf3f02d45a2907377c721db157f0
kaneflores <kaneflores911@gmail.com>
2026-04-18 14:43:02 -0400

    new file float

---
+++ float.c
@@ -0,0 +1,21 @@
+/* float.c */
+/*float
+// double 
+// %s for strings, %d for integers, %f for floats,
+
+
+
+*/ 
+
+#include <stdio.h>
+
+int main(){
+    // area
+    // PI * r^2
+    float radius, pi = 3.14159, area;
+
+    printf("What is the radius in cm? \n");
+    scanf("%f", &radius);
+
+    return 0;
+}
