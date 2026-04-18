262d568818d146b1933f35c643c348dbc958bac2
kaneflores <kaneflores911@gmail.com>
2026-04-18 15:36:39 -0400

    new code

--- if.c
+++ if.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    int x;
-    x = 50;
+    // int x;
+    // x = 50;
+
+    // if (x == 5){
+    //     printf("it is five\n");
+
+    // }
+
+    int velocity;
+
+    printf("HOw fast were you driving?\n");
+    scanf("%d", &velocity);
+    
+    if (velocity > 100){
+        printf("You are driving too fast!\n");
+    }
+    
+    else if(velocity>80){
+        printf("YOu are driving appropriately\n");
+    }
+
+    else{ //catch-all
+        printf("Okay. \n");
+    }
+
+    return -1;
 }
