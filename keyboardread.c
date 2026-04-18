05d151e3f9044dab06021e376f9c256769fb7cc0
kaneflores <kaneflores911@gmail.com>
2026-04-18 12:44:03 -0400

    new code

--- keyboardread.c
+++ keyboardread.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    char name[16]; // 16 characters allocated to store a name;
     printf("Hello, World! \n");
+
+    printf("Tell me your name... \n");
+    scanf("%s", name); // read a string from the user and store it in the variable 'name'
+
+    printf("Hello, %s! \n", name); 
     return -1;
 }
