4f9de53c2ef6839e7d9291db7674bac2bfbcd892
kaneflores <kaneflores911@gmail.com>
2026-04-26 13:06:27 -0400

    text editing

--- while.c
+++ while.c
@@ -12,13 +12,17 @@ int main(){
     //     return 0;
     // }
     //printf("testing you see this\n");
-    while (1){ // repeats indefinitely / endless loop 1 is like an if statement, this keeps happening unless x is changed
+    x=0; // still behaves the same just different implementation.
+    while (x!=1){ // repeats indefinitely / endless loop 1 is like an if statement, this keeps happening unless x is changed
         printf("Do you wish to quit? Press 1. \n");
         scanf("%d", &x);
         if (x ==1){
+            //break; skips outside main function
             return 0;
         }
     }
+
+    printf("Important!\n");
 
     return 0; // never reached, but used to end main function properly
 
