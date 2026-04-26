bf853df84fb6ff134218b9158afc91ea443a8b16
kaneflores <kaneflores911@gmail.com>
2026-04-26 12:56:49 -0400

    finished whilec

--- while.c
+++ while.c
@@ -2,9 +2,26 @@
 
 int main(){
     int x;
+    // while, do, for (types of loops)
 
 
-    printf("Do you wish to quit? Press 1. \n");
-    scanf("%d", &x);
-    
+    // printf("Do you wish to quit? Press 1. \n");
+    // scanf("%d", &x);
+
+    // if (x ==1){
+    //     return 0;
+    // }
+
+    while (1){ // repeats indefinitely
+        printf("Do you wish to quit? Press 1. \n");
+        scanf("%d", &x);
+        if (x ==1){
+            return 0;
+        }
+    }
+
+    return 0; // never reached, but used to end main function properly
+
+
+
 }
