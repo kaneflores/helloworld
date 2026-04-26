3d9d446585cd4befcaaed9b628dcd62e15d80830
kaneflores <kaneflores911@gmail.com>
2026-04-26 13:03:15 -0400

    just text editing

--- while.c
+++ while.c
@@ -12,7 +12,7 @@ int main(){
     //     return 0;
     // }
     //printf("testing you see this\n");
-    while (1){ // repeats indefinitely
+    while (1){ // repeats indefinitely / endless loop 1 is like an if statement, this keeps happening unless x is changed
         printf("Do you wish to quit? Press 1. \n");
         scanf("%d", &x);
         if (x ==1){
